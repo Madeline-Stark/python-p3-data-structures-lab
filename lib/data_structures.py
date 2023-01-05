9f029f6c712cde57805b05e9b8a0866e2afd1fa4
Madeline Stark <17558341+Madeline-Stark@users.noreply.github.com>
2023-01-04 17:50:21 -0800

    pass

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,4 @@
+import ipdb
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +18,40 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    # takes a list of spicy_foods 
+    # and returns a list of strings with the names of each spicy food.
+    return [food["name"] for food in spicy_foods]
+   
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    # returns a list of dictionaries where the heat level of the food is greater than 5.
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+   
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {food["heat_level"] * "🌶"}')
+    
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+    # takes a list of spicy_foods and a new spicy_food and returns the original list with the new spicy_food added.
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    # returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    print_spicy_foods(get_spiciest_foods(spicy_foods))
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_levels = [food["heat_level"] for food in spicy_foods] # grab all heat levels
+    total = sum(heat_levels)
+    return total / len(spicy_foods)
+    #returns an integer representing the average heat level of all the spicy foods in the array. 
+    # to derive the average of a collection, you need to calculate the total and divide number of elements in the collection.
+
